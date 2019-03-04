2ae612f1de74eb215d327244fea89f7267f18373
TrungBN <namtrung18101996@gmail.com>
2019-03-05 06:28:45 +0700

    Finish project

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  root 'sessions#new' 
+  root 'chats#index'
+  # root 'sessions#new'
   #temporary root path for now 
- 
-  resources :users, only:[:new, :create]
+  mount ActionCable.server => '/cable'
+  resources :users, only:[:new, :create] do
+    resources :chats, only: [:index, :show, :create]
+  end
   resources :sessions, only:[:new, :destroy, :create]
+  resources :messages, only:[:create]
 end
